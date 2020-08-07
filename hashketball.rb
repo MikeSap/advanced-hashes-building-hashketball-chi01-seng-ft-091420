ad027739896e2096a1eb0aadbac76f9f50886e4f
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-07 20:57:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,8 @@ require 'pry'
 # Write your code here!
 def game_hash
   game_hash = {
-    home: {:team_name, :colors, :players}, 
-    away: {:team_name, :colors, :players}
+    home: {:team_name =>"", :colors =>"", :players}, 
+    away: {:team_name :colors, :players}
   }
   binding.pry
 end
